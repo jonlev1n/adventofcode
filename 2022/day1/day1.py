1f8667bdb6067d185953fd9fb488bc9786c50574
Jonathan Levin <jmlevin78@gmail.com>
2022-12-01 00:15:48 -0500

    day 1

--- 2022/day1/day1.py
+++ 2022/day1/day1.py
@@ -1,6 +1,6 @@
 import os
 
-# from aocd import lines, submit
+from aocd import lines, submit
 
 test_data = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -10,3 +10,25 @@ def read_data():
     with open(os.path.join(__location__, "input.txt")) as input:
         data = input.read()
     return data
+
+
+def solve(input):
+    sums = []
+    cur_sum = 0
+    for line in input:
+        try:
+            cur_sum += int(line)
+        except:
+            sums.append(cur_sum)
+            cur_sum = 0
+
+    return sorted(sums)
+
+
+ans = solve(lines)
+pt1 = ans[len(ans) - 1]
+submit(pt1, part="a", day=1, year=2022)
+
+pt2 = ans[len(ans) - 1] + ans[len(ans) - 2] + ans[len(ans) - 3]
+print(pt2)
+submit(pt2, part="b", day=1, year=2022)
