2d4e9bb0d11c26669ea06171de6a1efd38035a5a
Jon Levin <jon@bluestonelogic.com>
2022-12-06 00:32:26 -0500

    day6

--- 2022/day6/day6.py
+++ 2022/day6/day6.py
@@ -10,3 +10,33 @@ def read_data():
     with open(os.path.join(__location__, "input.txt")) as input:
         data = input.read()
     return data
+
+
+def solve(input, packet_length):
+    for i in range(0, len(input)):
+        if i >= len(input) - packet_length:
+            break
+        substr = input[i : i + packet_length]
+        unique = list(set(substr))
+        if len(unique) == packet_length:
+            return i + packet_length
+
+
+data = get_data(day=6, year=2022)
+# test_data = [
+#     "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+#     "bvwbjplbgvbhsrlpgdmjqwftvncz",
+#     "nppdvjthqldpwncqszvftbrmjlhg",
+#     "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+#     "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+# ]
+
+# for t in test_data:
+#     print(solve_a(t, 14))
+
+
+ans1 = solve(data, 4)
+submit(ans1, day=6, year=2022, part="a")
+
+ans2 = solve(data, 14)
+submit(ans2, day=6, year=2022, part="b")
