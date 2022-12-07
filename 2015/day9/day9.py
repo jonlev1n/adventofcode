31f297326cb1373a516d65e1648a9016cfae56ac
Jonathan Levin <jmlevin78@gmail.com>
2022-12-07 09:59:11 -0500

    day 7 - jeeeeeeeeesus

--- 2015/day9/day9.py
+++ 2015/day9/day9.py
@@ -1,6 +1,6 @@
 import os
 from aocd import lines, submit, get_data
-
+import re, itertools
 
 test_data = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -10,3 +10,43 @@ def read_data():
     with open(os.path.join(__location__, "input.txt")) as input:
         data = input.read()
     return data
+
+
+def solve(input):
+    cities = []
+    dists = {}
+    for line in input:
+        res = re.findall(r"[\w]{3,}|[\d]+", line)
+        if not res[0] in dists.keys():
+            dists[res[0]] = {res[1]: int(res[2])}
+        else:
+            dists[res[0]] = {**dists[res[0]], res[1]: int(res[2])}
+        if not res[1] in dists.keys():
+            dists[res[1]] = {res[0]: int(res[2])}
+        else:
+            dists[res[1]] = {**dists[res[1]], res[0]: int(res[2])}
+
+        for i in range(0, 2):
+            if res[i] not in cities:
+                cities.append(res[i])
+
+    permutations = list(itertools.permutations(cities))
+    min_dist = -1
+    max_dist = 0
+    for p in permutations:
+        dist = 0
+        for i in range(0, len(p) - 1):
+            dist += dists[p[i]][p[i + 1]]
+        if min_dist < 0 or dist < min_dist:
+            min_dist = dist
+        if dist > max_dist:
+            max_dist = dist
+
+    return min_dist, max_dist
+
+
+data = get_data(day=9, year=2015)
+ans1 = solve(data.splitlines())[0]
+ans2 = solve(data.splitlines())[1]
+submit(ans1, day=9, year=2015, part="a")
+submit(ans2, day=9, year=2015, part="b")
--- 2022/day7/day7.py
+++ 2022/day7/day7.py
@@ -22,65 +22,75 @@ class _file:
 
 
 class _dir:
-    def __init__(self, name) -> None:
+    def __init__(self, name, parent=None) -> None:
         self.name = name
         self.children = []
         self.size = None
+        self.parent = parent
 
     def __str__(self):
         return "%s (dir, size = %s)" % (self.name, self.size)
 
 
-def solve_a(input):
+def solve(input):
     dirs = []
     cur_dir = None
     sizes_to_sum = []
+    total_disk = 70000000
+    space_needed = 30000000
+    dirs_to_delete = []
     for line in input:
-        if line == "$ cd .." or line == "$ ls":
+        # dont need to do anything here
+        if line == "$ ls":
             continue
         s = line.split()
+        # handle directory changes
         if "$ cd" in line:
             dir_name = s[len(s) - 1]
-            d = next((x for x in dirs if x.name == dir_name), None)
-            if not d:
-                d = _dir(name=dir_name)
-                dirs.append(d)
-            cur_dir = d
-        elif "$" not in line:
-            if "dir" in line:
-                dir_name = s[len(s) - 1]
-                if not any(d.name == dir_name for d in dirs):
-                    d = _dir(name=dir_name)
-                    dirs.append(d)
-                    cur_dir.children.append(d)
+            if dir_name == "..":
+                cur_dir = cur_dir.parent
             else:
-                file_name = s[len(s) - 1]
-                file_size = s[0]
-                f = _file(size=file_size, name=file_name)
-                cur_dir.children.append(f)
+                # only create dirs when you cd into them
+                d = _dir(name=dir_name, parent=cur_dir)
+                if cur_dir is not None:
+                    cur_dir.children.append(d)
+                dirs.append(d)
+                cur_dir = d
+        # handle file creation
+        elif "$" not in line and "dir" not in line:
+            file_name = s[len(s) - 1]
+            file_size = s[0]
+            f = _file(size=file_size, name=file_name)
+            cur_dir.children.append(f)
 
+    # get directory sizes
+    get_dir_sizes(dirs[0])  # seed with "/" - the recursion will take care of the rest
     for d in dirs:
-        d.size = get_size(d)
+        if d.name == "/":
+            total_disk_used = d.size
+            free_space = total_disk - total_disk_used
+            min_delete_size = space_needed - free_space
+        if d.size >= min_delete_size:
+            dirs_to_delete.append(d.size)
         if d.size <= 100000:
             sizes_to_sum.append(d.size)
-        print(d)
 
-    return sum(sizes_to_sum)
+    return sum(sizes_to_sum), min(dirs_to_delete)
 
 
-def get_size(dir):
+def get_dir_sizes(dir):
     # base case
     children = dir.children
     if all(c.size is not None for c in children):
         size = sum(c.size for c in dir.children)
-        return size
-
+        dir.size = size
     else:
         s = sum(c.size for c in children if c.size is not None)
         dirs = [c for c in children if c.size is None]
         for d in dirs:
-            s += get_size(d)
-        return s
+            get_dir_sizes(d)
+            s += d.size
+        dir.size = s
 
 
 data = lines
@@ -109,7 +119,9 @@ test_data = [
     "5626152 d.ext",
     "7214296 k",
 ]
-test_ans = solve_a(test_data)
+# test_ans = solve(test_data)
 # print(test_ans)
-# ans1 = solve_a(data)
-# submit(ans1, day=7, year=2022, part="a")
+ans1 = solve(data)[0]
+submit(ans1, day=7, year=2022, part="a")
+ans2 = solve(data)[1]
+submit(ans2, day=7, year=2022, part="b")
