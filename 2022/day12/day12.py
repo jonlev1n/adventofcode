aecebd72cf5456bbccc128ca544e3cff52c94adb
Jonathan Levin <jmlevin78@gmail.com>
2022-12-12 02:59:05 -0500

    day 12

--- 2022/day12/day12.py
+++ 2022/day12/day12.py
@@ -1,6 +1,6 @@
 import os
 from aocd import lines, submit, get_data
-import string, numpy as np
+import string, numpy as np, networkx as nx
 
 test_data = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -12,70 +12,24 @@ def read_data():
     return data
 
 
-def get_start_finish(input):
-    for i in range(0, len(input)):
-        for j in range(0, len(input[i])):
-            if input[i][j] == "S":
-                start = (i, j)
-            if input[i][j] == "E":
-                end = (i, j)
-    return (start, end)
+def solve(input):
 
+    G = nx.DiGraph()
+    H = np.array([[*x.strip()] for x in input])
 
-def get_elevation(e):
-    if e == "S":
-        return get_elevation("a")
-    if e == "E":
-        return get_elevation("z")
-    return string.ascii_letters.index(e)
+    S = tuple(*np.argwhere(H == "S"))
+    H[S] = "a"
+    E = tuple(*np.argwhere(H == "E"))
+    H[E] = "z"
 
+    N = nx.grid_2d_graph(*H.shape).to_directed()
 
-def get_neighbors(input, coords, elevation):
-    # surround = [(-1, 1), (-1, 0), (-1, -1), (0, 1), (0, -1), (1, 1), (1, 0), (1, -1)]
-    surround = [
-        (1, 0),
-        (-1, 0),
-        (0, -1),
-        (0, 1),
-    ]  # ordering this way prioritizes going forward
-    s_coords = [tuple(np.add(coords, s)) for s in surround]
-    # neighbors = {}
-    neighbors = []
-    for s in s_coords:
-        if s[0] in range(0, len(input)) and s[1] in range(0, len(input[0])):
-            n = input[s[0]][s[1]]
-            if get_elevation(n) - elevation <= 1:
-                neighbors.append((s[0], s[1]))
-                # if n in neighbors.keys():
-                #     neighbors[n].append((s[0], s[1]))
-                # else:
-                #     neighbors[n] = [(s[0], s[1])]
-    return neighbors
+    for a, b in N.edges():
+        if ord(H[b]) <= ord(H[a]) + 1:
+            G.add_edge(a, b)
 
-
-def solve(c, visited):
-    print(c)
-    while c != end:
-        e = get_elevation(input[c[0]][c[1]])
-        n = get_neighbors(input, c, e)
-        if len(n) > 1:
-            for new_c in n:
-                solve(new_c, visited)
-        if len(n) == 0 or all(new_c in visited for new_c in n):
-            # dead end condition
-            print("dead ending!")
-            return "dead end"
-
-        # pick the next one
-        # move to the first one that you havent already been to?
-        for new_c in n:
-            if new_c not in visited:
-                c = new_c
-
-        visited.append(c)
-
-    visited.append(end)
-    return len((list(set(visited))))
+    p = nx.shortest_path_length(G, target=E)
+    return (p[S], min(p[a] for a in p if H[a] == "a"))
 
 
 test_data = [
@@ -85,17 +39,7 @@ test_data = [
     "acctuvwj",
     "abdefghi",
 ]
-# test_ans = solve(test_data)
-# print(test_ans)x
 
-(start, end) = get_start_finish(test_data)
-visited = []
-c = start
-visited = [c]
-x = solve(c, visited)
-
-
-# ans1 = solve(lines)
-# submit(ans1, day=12, year=2022, part="a")
-# ans2 = solve(lines)
-# submit(ans2, day=12, year=2022, part="b")
+(ans1, ans2) = solve(lines)
+submit(ans1, day=12, year=2022, part="a")
+submit(ans2, day=12, year=2022, part="b")
