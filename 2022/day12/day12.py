aaf1ae80929b93e8e6de0709baad655d42657cdf
Jonathan Levin <jmlevin78@gmail.com>
2022-12-13 11:01:10 -0500

    day 13

--- 2022/day12/day12.py
+++ 2022/day12/day12.py
@@ -1,6 +1,6 @@
 import os
 from aocd import lines, submit, get_data
-import string, numpy as np, networkx as nx
+import string, numpy as np, networkx as nx, math
 
 test_data = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -12,24 +12,86 @@ def read_data():
     return data
 
 
+def get_start_end(input):
+    for i in range(0, len(input)):
+        for j in range(0, len(input[i])):
+            if input[i][j] == "S":
+                start = (i, j)
+            if input[i][j] == "E":
+                end = (i, j)
+
+    return (start, end)
+
+
+def get_as(input):
+    all_a = []
+    for i in range(0, len(input)):
+        for j in range(0, len(input[i])):
+            if input[i][j] == "a":
+                all_a.append((i, j))
+    return all_a
+
+
+def get_elevation(e):
+    if e == "S":
+        return get_elevation("a")
+    if e == "E":
+        return get_elevation("z")
+    return string.ascii_letters.index(e)
+
+
+def get_connected(input, coords):
+    c = input[coords[0]][coords[1]]
+    cur_elevation = get_elevation(c)
+    surround = [
+        (1, 0),
+        (-1, 0),
+        (0, -1),
+        (0, 1),
+    ]
+    s_coords = [tuple(np.add(coords, s)) for s in surround]
+    connected = []
+    for s in s_coords:
+        if s[0] in range(0, len(input)) and s[1] in range(0, len(input[0])):
+            n = input[s[0]][s[1]]
+            if get_elevation(n) - cur_elevation <= 1:
+                connected.append((s[0], s[1]))
+    return connected
+
+
 def solve(input):
 
-    G = nx.DiGraph()
-    H = np.array([[*x.strip()] for x in input])
+    (start, end) = get_start_end(input)
+    all_a = get_as(input)
+    all_a.insert(0, start)
+    steps = []
+    for start in all_a:
+        dists = {}
+        # distance to all other nodes should be infinite
+        for i in range(0, len(input)):
+            for j in range(0, len(input[0])):
+                dists[(i, j)] = math.inf
 
-    S = tuple(*np.argwhere(H == "S"))
-    H[S] = "a"
-    E = tuple(*np.argwhere(H == "E"))
-    H[E] = "z"
+        dists[start] = 0
 
-    N = nx.grid_2d_graph(*H.shape).to_directed()
-
-    for a, b in N.edges():
-        if ord(H[b]) <= ord(H[a]) + 1:
-            G.add_edge(a, b)
-
-    p = nx.shortest_path_length(G, target=E)
-    return (p[S], min(p[a] for a in p if H[a] == "a"))
+        visited = []
+        queue = [start]
+        while len(queue) != 0:
+            current = queue.pop(0)
+            if current in visited:
+                continue
+            visited.append(current)
+            connected = get_connected(input, current)
+            for c in connected:
+                if c not in visited:
+                    queue.append(c)
+                cur_dist = dists[current]
+                new_dist = 1 + cur_dist
+                connected_dist = dists[(c[0], c[1])]
+                if new_dist < connected_dist:
+                    dists[c[0], c[1]] = new_dist
+        steps.append(dists[end])
+    return (steps[0], min(steps))
 
 
 test_data = [
@@ -40,6 +102,7 @@ test_data = [
     "abdefghi",
 ]
 
+# print(solve(test_data))
 (ans1, ans2) = solve(lines)
 submit(ans1, day=12, year=2022, part="a")
 submit(ans2, day=12, year=2022, part="b")
--- 2022/day13/day13.py
+++ 2022/day13/day13.py
@@ -1,5 +1,6 @@
 import os
 from aocd import lines, submit, get_data
+from functools import cmp_to_key
 
 
 test_data = []
@@ -10,3 +11,60 @@ def read_data():
     with open(os.path.join(__location__, "input.txt")) as input:
         data = input.read()
     return data
+
+
+def compare(a, b):
+    if isinstance(a, int):
+        if isinstance(b, int):
+            if a < b:
+                return 1
+            if b < a:
+                return -1
+            return 0
+        return compare([a], b)
+    if isinstance(b, int):
+        return compare(a, [b])
+
+    for si in range(min((len(a), len(b)))):
+        sr = compare(a[si], b[si])
+        if 1 == sr:
+            return 1
+        if -1 == sr:
+            return -1
+
+    if len(a) < len(b):
+        return 1
+    if len(b) < len(a):
+        return -1
+
+    return 0
+
+
+def solve(input):
+    packets = []
+    correct = 0
+    for idx, pair in enumerate(input.split("\n\n")):
+        l, r = map(eval, pair.split("\n"))
+        packets.append(l)
+        packets.append(r)
+        if compare(l, r) == 1:
+            correct += idx + 1
+
+    decoder = 1
+    packets.append([[2]])
+    packets.append([[6]])
+    packets = sorted(packets, key=cmp_to_key(compare), reverse=True)
+    for idx, packet in enumerate(packets):
+        kk = ascii(packet)
+        if "[[2]]" == kk:
+            decoder *= idx + 1
+        if "[[6]]" == kk:
+            decoder *= idx + 1
+
+    return correct, decoder
+
+
+data = get_data(day=13, year=2022)
+ans1, ans2 = solve(data)
+submit(ans1, day=13, year=2022, part="a")
+submit(ans2, day=13, year=2022, part="b")
