1ac81f0730760654b4bdb053170f546d06681fac
Jon Levin <jon@bluestonelogic.com>
2023-12-01 18:31:33 -0500

    2023 setup

---
+++ 2023/day16/day16.py
@@ -0,0 +1,11 @@
+import os
+from aocd import lines, submit, get_data
+
+
+test_data = []
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def read_data():
+	with open(os.path.join(__location__, 'input.txt')) as input:
+		data = input.read()
+	return data
