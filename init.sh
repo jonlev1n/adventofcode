ffe2ebac08d7332980df89e9d846eee27a1985ea
Jonathan Levin <jmlevin78@gmail.com>
2023-12-15 23:44:30 -0500

    modifying to use the aocd library and, dont bother with reading from file

--- init.sh
+++ init.sh
@@ -8,40 +8,55 @@
 #   |- ...
 
 # get the current year
-if [ $# -eq 0 ]
-then
+if [ $# -eq 0 ]; then
 	YEAR=$(date "+ %Y")
 	YEAR=${YEAR// /}
 else
 	YEAR=$1
 fi
 
-if [ ! -d "$YEAR" ]
-then 
+if [ ! -d "$YEAR" ]; then
 	mkdir $YEAR
-else 
+else
 	echo "This year is already set up!"
 	exit 0
 fi
 
 cd "$YEAR"
 
-for i in {1..25}
-do
- 	mkdir "day$i"
+for i in {1..25}; do
+	mkdir "day$i"
 	touch "day$i/__init__.py"
 	touch "day$i/day$i.py"
 	touch "day$i/input.txt"
-	echo "import os
+	echo "
 from aocd import lines, submit, get_data
 
 
 test_data = []
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-def read_data():
-	with open(os.path.join(__location__, 'input.txt')) as input:
-		data = input.read()
-	return data" >> "day$i/day$i.py" 
-done    	
-	
+
+def part1():
+    data = test_data
+    # data = lines
+
+    ans1 = None
+    return ans1
+
+
+def part2():
+    data = test_data
+    # data = lines
+
+    ans2 = None
+    return ans2
+
+
+ans1 = part1()
+ans2 = part2()
+
+# uncomment these lines to submit
+# submit(ans1, part=\"a\", day=${i}, year=${YEAR})
+# submit(ans2, part=\"b\", day=${i}, year=${YEAR})
+" >>"day${i}/day${i}.py"
+done
