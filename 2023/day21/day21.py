514768a104f5b391f63c4f14d7f17ab4ffb1e2cf
Jonathan Levin <jmlevin78@gmail.com>
2023-12-19 02:02:24 -0500

    setting up remainder of days

--- 2023/day21/day21.py
+++ 2023/day21/day21.py
@@ -1,11 +1,28 @@
-import os
 from aocd import lines, submit, get_data
 
 
 test_data = []
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-def read_data():
-	with open(os.path.join(__location__, 'input.txt')) as input:
-		data = input.read()
-	return data
+
+def part1():
+    data = test_data
+    # data = lines
+
+    ans1 = None
+    return ans1
+
+
+def part2():
+    data = test_data
+    # data = lines
+
+    ans2 = None
+    return ans2
+
+
+ans1 = part1()
+ans2 = part2()
+
+# uncomment these lines to submit
+# submit(ans1, part="a", day=21, year=2023)
+# submit(ans2, part="b", day=21, year=2023)
