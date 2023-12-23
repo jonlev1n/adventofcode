0c3ac5c3912094caf450c7746cbacd9ee3bfda1f
Jonathan Levin <jmlevin78@gmail.com>
2023-12-23 02:07:31 -0500

    var change

--- 2023/day22/day22.py
+++ 2023/day22/day22.py
@@ -1,28 +1,104 @@
 from aocd import lines, submit, get_data
 
 
-test_data = []
+def simulateFall(bricks):
+    n = len(bricks)
+
+    supports = {i: [] for i in range(n)}
+    supportedBy = {i: [] for i in range(n)}
+
+    settled = {}
+
+    for brick in bricks:
+        end1, end2, bId = brick
+        (x1, y1, z1), (x2, y2, z2) = end1, end2
+
+        supporters = []
+        while z1 > 1 and len(supporters) == 0:
+            for x in range(x1, x2 + 1):
+                for y in range(y1, y2 + 1):
+                    for z in range(z1, z2 + 1):
+                        if (x, y, z - 1) in settled:
+                            supporters.append((x, y, z - 1))
+
+            if len(supporters) == 0:
+                z1 -= 1
+                z2 -= 1
+
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                for z in range(z1, z2 + 1):
+                    settled[(x, y, z)] = bId
+
+        for sId in set(settled[sup] for sup in supporters):
+            supports[sId].append(bId)
+            supportedBy[bId].append(sId)
+
+    return supports, supportedBy
+
+
+def countRemovable(supports, supportedBy):
+    count = 0
+    for bId in supports:
+        canBeRemoved = True
+        for sId in supports[bId]:
+            if len(supportedBy[sId]) == 1:
+                canBeRemoved = False
+                break
+
+        if canBeRemoved:
+            count += 1
+
+    return count
+
+
+def countChainReaction(supports, supportedBy):
+    count = 0
+    for bId in supports:
+        toCheck = supports[bId].copy()
+        falling = {bId}
+
+        while toCheck:
+            curId = toCheck.pop(0)
+
+            if all(sId in falling for sId in supportedBy[curId]):
+                falling.add(curId)
+                toCheck += supports[curId]
+
+        count += len(falling) - 1
+
+    return count
+
+
+bricks = []
+i = 0
+for line in lines:
+    brick = [[int(n) for n in end.split(",")] for end in line.strip().split("~")]
+    brick += [i]
+
+    bricks.append(brick)
+    i += 1
+bricks.sort(key=lambda b: b[0][2])
 
 
 def part1():
-    data = test_data
-    # data = lines
+    supports, supportedBy = simulateFall(bricks)
+    ans1 = countRemovable(supports, supportedBy)
 
-    ans1 = None
+    print(ans1)
     return ans1
 
 
 def part2():
-    data = test_data
-    # data = lines
+    supports, supportedBy = simulateFall(bricks)
+    ans2 = countChainReaction(supports, supportedBy)
 
-    ans2 = None
+    print(ans2)
     return ans2
 
 
 ans1 = part1()
 ans2 = part2()
 
-# uncomment these lines to submit
-# submit(ans1, part="a", day=22, year=2023)
-# submit(ans2, part="b", day=22, year=2023)
+submit(ans1, part="a", day=22, year=2023)
+submit(ans2, part="b", day=22, year=2023)
--- 2023/day23/day23.py
+++ 2023/day23/day23.py
@@ -1,12 +1,93 @@
 from aocd import lines, submit, get_data
 
 
-test_data = []
+test_data = [
+    "#.#####################",
+    "#.......#########...###",
+    "#######.#########.#.###",
+    "###.....#.>.>.###.#.###",
+    "###v#####.#v#.###.#.###",
+    "###.>...#.#.#.....#...#",
+    "###v###.#.#.#########.#",
+    "###...#.#.#.......#...#",
+    "#####.#.#.#######.#.###",
+    "#.....#.#.#.......#...#",
+    "#.#####.#.#.#########v#",
+    "#.#...#...#...###...>.#",
+    "#.#.#v#######v###.###v#",
+    "#...#.>.#...>.>.#.###.#",
+    "#####v#.#.###v#.#.###.#",
+    "#.....#...#...#.#.#...#",
+    "#.#########.###.#.#.###",
+    "#...###...#...#...#.###",
+    "###.###.#.###v#####v###",
+    "#...#...#.#.>.>.#.>.###",
+    "#.###.###.#.###.#.#v###",
+    "#.....###...###...#...#",
+    "#####################.#",
+]
+
+D = {
+    "^": (-1, 0),
+    "v": (1, 0),
+    "<": (0, -1),
+    ">": (0, 1),
+}
+
+
+S = []
+Q = []
+
+
+def traverse(c, steps, data):
+    cy, cx = c
+    p = data[cy][cx]
+    if p in D.keys():
+        next_c = [(cy + D[p][0], cx + D[p][1])]
+    else:
+        next_c = [
+            (cy + dy, cx + dx)
+            for (dy, dx) in D.values()
+            if (
+                cy + dy in range(0, len(data))
+                and cx + dx in range(0, len(data[0]))
+                and data[(cy + dy)][(cx + dx)] in list(D.keys()) + ["."]
+                and (cy + dy, cx + dx) not in steps
+                and not (
+                    data[(cy + dy)][(cx + dx)] in D.keys()
+                    and (
+                        dy + D[data[(cy + dy)][(cx + dx)]][0],
+                        dx + D[data[(cy + dy)][(cx + dx)]][1],
+                    )
+                    == (0, 0)
+                )
+            )
+        ]
+
+    if len(next_c) > 1:
+        Q.insert(0, steps.copy())
+
+    if all([c in steps for c in next_c]):
+        return steps
+    else:
+        for c in next_c:
+            # steps = Q.pop()
+            steps.append(c)
+            # print("steps:", steps)
+            print(c, next_c)
+            return traverse(c, steps, data)
 
 
 def part1():
     data = test_data
     # data = lines
+
+    sy, sx = (0, 1)
+    steps = [(sy, sx)]
+    # ey, ex = (len(data) - 1, len(data) - 2)
+
+    S = traverse((sy, sx), steps, data)
+    print("S:", S)
 
     ans1 = None
     return ans1
